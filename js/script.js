d42833801eca3ac222d2485a95d1ba23eb301c18
amimul21 <amimulit21@gmail.com>
2021-09-06 15:14:27 +0600

    filterizr

--- js/script.js
+++ js/script.js
@@ -9,4 +9,26 @@ $(function() {
   $('.gallery-navs a').on("click", function (e) {
     e.preventDefault();
   });
+
+
+  /*wow js initialization*/
+  
 });
+
+var nav_item = $('.nav-item');
+nav_item.on("click", function (e) {
+    nav_item.removeClass("active");
+
+    $(this).addClass("active");
+    $('.gallery').css({"background":"#8aa7c4", "padding" : "5px"});
+  
+});
+
+wow = new WOW ({
+    animateClass: 'animated',
+    offset: 100,
+    callback:     function(box) {
+      console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+  });
+  wow.init();
